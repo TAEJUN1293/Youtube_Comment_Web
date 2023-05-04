b7dce97a354cbbb23e4253cddc1a891788b7579d
ksh1357 <sanghee1357@naver.com>
2023-05-05 02:49:27 +0900

    Model 생성 및 예제 데이터 추가

---
+++ youtube_comment_web/youtube_comment/views.py
@@ -0,0 +1,16 @@
+from rest_framework.decorators import api_view
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import *
+
+# Create your views here.
+def main(request):
+    all_video = Video.objects.all()
+    context = {'first_video': all_video[0].data}
+    return render(request, 'youtube_comment/main.html', context)
+
+def detail(request):
+    all_video = Video.objects.all()
+    context = {'first_video': all_video[0].data}
+    return render(request, 'youtube_comment/detail.html', context)
