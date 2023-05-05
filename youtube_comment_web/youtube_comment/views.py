7591f10ff45382a0fa513bc1b2b05771f7d80cad
ksh1357 <sanghee1357@naver.com>
2023-05-05 20:42:07 +0900

    front 최종

--- youtube_comment_web/youtube_comment/views.py
+++ youtube_comment_web/youtube_comment/views.py
@@ -11,9 +11,18 @@ def main(request):
     return render(request, 'youtube_comment/main.html', context)
 
 def detail(request):
-    category = request.GET.get('category', None)
+    category_id = request.GET.get('category', None)
+    try:
+        category = Category.objects.get(pk=category_id)
+    except Category.DoesNotExist:
+        category = None
     category_list = Category.objects.all()
-    video_list = Video.objects.all()
-    wordcloud_list = WordCloud.objects.all()
-    context = {'category_list': category_list, 'category': category, 'video_list': video_list, 'wordcloud_list': wordcloud_list}
+    video_list = []
+    videos = Video.objects.filter(categories=category_id)
+    for video in videos:
+        comment_list = Comment.objects.filter(video_id=video)
+        keyword = Keyword.objects.get(video_id=video)
+        keyword_list = keyword.keyword.split(' ')
+        video_list.append([video, comment_list, keyword_list])
+    context = { 'category': category, 'category_list': category_list, 'video_list': video_list}
     return render(request, 'youtube_comment/detail.html', context)
