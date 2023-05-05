84b14b0bac21b042e67729d250099fe5cbaefb67
ksh1357 <sanghee1357@naver.com>
2023-05-05 16:04:44 +0900

    수정사항 반영

--- youtube_comment_web/youtube_comment/views.py
+++ youtube_comment_web/youtube_comment/views.py
@@ -6,11 +6,14 @@ from .models import *
 
 # Create your views here.
 def main(request):
-    all_video = Video.objects.all()
-    context = {'first_video': all_video[0]}
+    cloud_list = WordCloud.objects.all()
+    context = {'word_clouds': cloud_list}
     return render(request, 'youtube_comment/main.html', context)
 
 def detail(request):
-    all_video = Video.objects.all()
-    context = {'first_video': all_video[0].data}
+    category = request.GET.get('category', None)
+    category_list = Category.objects.all()
+    video_list = Video.objects.all()
+    wordcloud_list = WordCloud.objects.all()
+    context = {'category_list': category_list, 'category': category, 'video_list': video_list, 'wordcloud_list': wordcloud_list}
     return render(request, 'youtube_comment/detail.html', context)
